99fe4b1b076b0122bb1b8ddeceb0bea685099352
Billy <billdev1958@gmail.com>
2023-09-23 15:10:25 -0600

    gh-pages-deploy

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {
-  HashRouter,
-  RouterProvider
+  RouterProvider,
+  createHashRouter
 } from "react-router-dom"
 import Homepage from './pages/Homepage';
 import Soluciones from './pages/Soluciones';
@@ -11,7 +11,7 @@ import Proyects from './pages/Proyects';
 import Contact from './pages/Contact';
 
 
-const router = HashRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App/>,
